a3767ea4a4d764aa9f8a234c2905dfaefbbce9b4
hlinforest <hllin@sfu.ca>
2015-05-18 14:01:34 -0700

    change

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ $(document).ready(function(){
 
 	$(".s1").one("click", function(){
 
-		$(this).append("&nbsp; (you are on this step)");
-
+		//$(this).append("&nbsp; (you are on this step)");
+		$(this).append("<p>(you are on this step)</p>");
+		$(".s1").not(this).remove("<p>");
 	});
 });
 
