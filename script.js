9fdc042682a8cb77ddf7429a90b9b0ddb98f3378
hlinforest <hllin@sfu.ca>
2015-05-18 14:15:53 -0700

    update

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ $(document).ready(function(){
 	$(".s1").one("click", function(){
 
 		//$(this).append("&nbsp; (you are on this step)");
-		$(this).append("<p>(you are on this step)</p>");
-		$(".s1").not(this).remove("<p>");
+		//$(this).append("<p>(you are on this step)</p>");
+		$(".s1").not(this).fadeTo("slow", "0.5");
+		//$(this).fadeIn();
 	});
 });
 
